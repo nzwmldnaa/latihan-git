5c31f22b3015a5b71d7fc5a4d7addda36cbf3989
Nazwa Maulidina <nzwmldnaaa@gmail.com>
2025-12-09 23:12:05 +0700

    feat: tambahkan output kedua pada program

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class Hello {
     public static void main(String[] args) {
         //menampilkan pesan ke layar
         String msg = "Hello World";
+        
         System.out.println(message);
+        System.out.println("Nama saya nazwa");
+
     }
 }
