7aa3c326038625107de4390195c2763c75cf4c55
Nazwa Maulidina <nzwmldnaaa@gmail.com>
2025-12-14 14:08:30 +0700

    menambahkan file selamat belajar

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Pesan pesan1 = new Pesan("Halo, dunia!");
+        pesan1.tampilkan();
+
+        pesan1.setIsi("Selamat belajar Java!");
+        pesan1.tampilkan();
+    }
+}    
+}
